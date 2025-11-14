ae8e41712134fc846085c6954f283cff25ccc5ff
UttamChaudhary12 <cuttam37@gmail.com>
2025-11-14 22:13:12 +0545

    Add index.html, buttons.html, and script.js

---
+++ script.js
@@ -0,0 +1,11 @@
+function calculateNum(){
+    let a = 5; 
+    let b = 6; 
+    c = a + b; 
+    document.getElementById("second").innerHTML = "The result is: " + c; 
+}
+
+function hideText(){
+    document.getElementById("first").style.display = "none"; 
+}
+
